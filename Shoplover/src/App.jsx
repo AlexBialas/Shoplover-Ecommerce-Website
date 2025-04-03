b5eb6619953b9b4fbad3de43f380399904bb2cf4
dci-student <aleksandrabialas94@gmail.com>
2025-04-03 12:55:56 +0200

    added slider

--- Shoplover/src/App.jsx
+++ Shoplover/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Nav from "../src/components/Nav/Nav";
+import Hero from "../src/components/Hero/Hero";
 
 function App() {
   return (
     <>
       <Nav />
+      <Hero />
     </>
   );
 }
---
+++ Shoplover/src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Wo1 from "../../components/Hero/wo1.png";
+
+const Hero = () => {
+  return (
+    <div>
+      <img src={Wo1} alt="Wo1" className="w-100" />
+    </div>
+  );
+};
+
+export default Hero;
