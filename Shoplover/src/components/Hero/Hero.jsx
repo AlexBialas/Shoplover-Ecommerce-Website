d6ae5b6d8a12f57857604461571eaedae924adab
dci-student <aleksandrabialas94@gmail.com>
2025-04-03 13:58:28 +0200

    added slider

--- Shoplover/src/components/Hero/Hero.jsx
+++ Shoplover/src/components/Hero/Hero.jsx
@@ -1,10 +1,109 @@
 import React from "react";
 import Wo1 from "../../components/Hero/wo1.png";
+import Wo2 from "../../components/Hero/wo2.png";
+import Wo3 from "../../components/Hero/wo3.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const Hero = () => {
+const ImageList = [
+  {
+    id: 1,
+    img: Wo1,
+    title: "Upto 50% off on all Men's Wear",
+    description:
+      "lorem His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 2,
+    img: Wo2,
+    title: "30% off on all Women's Wear",
+    description:
+      "Who's there lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 3,
+    img: Wo3,
+    title: "70% off on all Products Sale",
+    description:
+      "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
+
+const Hero = ({ handleOrderPopup }) => {
+  var settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: "ease-in-out",
+    pauseOnHover: false,
+    pauseOnFocus: true,
+  };
+
   return (
-    <div>
-      <img src={Wo1} alt="Wo1" className="w-100" />
+    <div className="relative overflow-hidden min-h-[400px] sm:min-h-[500px] bg-gray-100 flex justify-center items-center duration-200">
+      {/* background pattern */}
+      <div className="h-[600px] w-[600px] bg-orange-500/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-10"></div>
+      {/* hero section */}
+      <div className="container pb-8 sm:pb-0">
+        <Slider {...settings}>
+          {ImageList.map((data) => (
+            <div key={data.id}>
+              <div className="grid grid-cols-1 sm:grid-cols-2">
+                {/* text content section */}
+                <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                  <h1
+                    data-aos="zoom-out"
+                    data-aos-duration="500"
+                    data-aos-once="true"
+                    className="text-4xl sm:text-5xl lg:text-6xl font-bold"
+                  >
+                    {data.title}
+                  </h1>
+                  <p
+                    data-aos="fade-up"
+                    data-aos-duration="500"
+                    data-aos-delay="100"
+                    className="text-sm sm:text-base"
+                  >
+                    {data.description}
+                  </p>
+                  <div
+                    data-aos="fade-up"
+                    data-aos-duration="500"
+                    data-aos-delay="300"
+                  >
+                    <button
+                      onClick={handleOrderPopup}
+                      className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full"
+                    >
+                      Order Now
+                    </button>
+                  </div>
+                </div>
+                {/* image section */}
+                <div className="order-1 sm:order-2 flex justify-center items-center">
+                  <div
+                    data-aos="zoom-in"
+                    data-aos-once="true"
+                    className="relative z-10"
+                  >
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="max-w-full h-[250px] sm:h-[400px] object-contain mx-auto"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
